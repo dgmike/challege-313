8f9d1a6a43c79c767a3e343cdf609020749fd705
Michael Granados <michaelgranados@gmail.com>
2017-12-02 05:20:39 -0200

    refactoring links on collection mapper
    Signed-off-by: Michael Granados <michaelgranados@gmail.com>

--- src/mapper/collection_mapper.rb
+++ src/mapper/collection_mapper.rb
@@ -1,11 +1,15 @@
 class CollectionMapper
 	def self.links(base_url, page, limit, total)
+		last_page = (total / limit.to_f).ceil 
+		next_page = last_page > page
+		prev_page = page > 1
+
 		{
 			self: "#{base_url}?page=#{page}&limit=#{limit}",
-			next: page >= (total / limit.to_f).ceil ? nil : "#{base_url}?page=#{page + 1}&limit=#{limit}",
-			previous: page <= 1 ? nil : "#{base_url}?page=#{page - 1}&limit=#{limit}",
+			next: next_page && "#{base_url}?page=#{page + 1}&limit=#{limit}",
+			previous: prev_page && "#{base_url}?page=#{page - 1}&limit=#{limit}",
 			first: "#{base_url}?page=1&limit=#{limit}",
-			last: "#{base_url}?page=#{(total / limit.to_f).ceil}&limit=#{limit}",
+			last: "#{base_url}?page=#{last_page}&limit=#{limit}",
 		}
 	end
 end
